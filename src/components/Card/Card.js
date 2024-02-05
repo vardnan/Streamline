875c6243d71bf1cc0460c63ac78908f59692bfa6
vardnan <vardnansivarajah@gmail.com>
2024-02-05 13:47:12 -0500

    refine responsiveness, adjust colour styles, add priority number, and fix font family issues on safari

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,14 @@ import { React } from 'react';
 import { Link } from 'react-router-dom';
 import './Card.css';
 
-const Card = ({ identifier, blockColor, path, priority, category }) => {
+const Card = ({
+  identifier,
+  blockColor,
+  path,
+  priority,
+  category,
+  priorityNumber,
+}) => {
   return (
     <Link
       className="card"
@@ -22,6 +29,16 @@ const Card = ({ identifier, blockColor, path, priority, category }) => {
         >
           {priority}
         </p>
+        <div
+          className="priorityNumber"
+          style={{
+            backgroundColor:
+              priority === 'Important & urgent' ? 'white' : blockColor,
+            color: priority === 'Important & urgent' ? blockColor : 'white',
+          }}
+        >
+          {priorityNumber}
+        </div>
       </div>
       <div
         className="color-block"
